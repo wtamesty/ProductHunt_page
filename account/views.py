d69adaac25330f709bfa71b194ec9e7fb0529e7a
William Torres <wtamesty@hotmail.com>
2020-01-07 20:37:14 -0600

    the authentication is done

---
+++ account/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'account/signup.html', {'error':'Username has already been taken :('})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'account/signup.html', {'error':'Passwords don\'t match'})
+    else:
+        return render(request, 'account/signup.html')
+
+def login(request):
+    #return render(request, 'account/login.html', {'error':'Username or Password is incorrect.'})
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'], password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'account/login.html', {'error':'Username or Password is incorrect.'})
+    else:
+        return render(request, 'account/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
